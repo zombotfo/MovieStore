a85b76703451deece32bf28b60b922604cd1cb19
peach <peachlord567@gmail.com>
2026-03-28 23:33:45 +0700

    update project

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: "https://your-app.vercel.app"
+  origin: "https://movie-store-git-main-teerapats-projects-3fd85181.vercel.app"
 }));
 app.use(express.json());
 app.use("/auth", authRoutes);
